f441246a6e2b77b4a10ad6c4cf1362031858ef96
Sunteang S <sereysunteang@gmail.com>
2024-07-08 13:11:46 +0700

    commit added new change

--- example1.js
+++ example1.js
@@ -32,3 +32,25 @@ function square(number) {
 }
 
 console.log(processArrayToSquare(numbers, square));
+
+// ex3:
+
+const numbers = [2, 4, 6, 8, 10];
+
+function toSquare(numbers, callback) {
+  const result = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (callback(numbers[i])) {
+      result.push(numbers[i]);
+    }
+  }
+  return result;
+}
+
+function square(number) {
+  return number * number;
+}
+
+const arrToSquare = toSquare(numbers, square);
+console.log(arrToSquare);
